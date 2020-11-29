b89d5a4909c46ff848c0b5489ca377a5f9fd95a5
Camila <camila_ppg7@hotmail.com>
2020-11-29 11:20:52 -0300

    Exercício Resolvido

---
+++ Exercicio01/main.js
@@ -0,0 +1,13 @@
+  var nome = 'Camila Rodrigues'
+  var objn = new String(nome)
+  var idade = '32'
+  if ( idade > 18 )
+    console.log("Maior de idade")
+  else 
+    console.log("Menor de idade")
+  var obji = new String(idade)
+  console.log(idade)
+  console.log(typeof objn)
+  console.log(nome)
+  console.log(typeof obji)
+  
