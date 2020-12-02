2fc6257782bdc0e6080a60a998bb4387283be4f1
Camila <camila_ppg7@hotmail.com>
2020-12-02 20:18:28 -0300

    Exercício11 Resolvido

--- Exercicio11/main.js
+++ Exercicio11/main.js
@@ -1,1 +1,14 @@
-// Faça a tarefa
+function main()
+{
+ var num = parseInt(document.getElementById("num").value);
+ var resp = document.getElementById("resposta");
+
+ resp.innerHTML = fatorial(num);
+}
+function fatorial(x)
+{
+ if(x<=1)
+  return 1;
+ else
+  return x * fatorial(x-1);
+}
