91e0265a7ad87a7f6a06de6e44e840e713df5e27
Camila <camila_ppg7@hotmail.com>
2020-12-02 20:15:05 -0300

    Exercício10 Resolvido

--- Exercicio10/main.js
+++ Exercicio10/main.js
@@ -2,6 +2,25 @@ const tag = document.querySelector('input[name="temperatura"]')
 const resultados = document.querySelector('div.resultados')
 
 tag.addEventListener('blur', function(evento) {
-    // Continue a tarefa aqui...
     
 })
+function main()
+{
+ var num = parseInt(document.getElementById("num").value);
+ var resp = document.getElementById("resposta");
+ 
+ if(document.getElementById("CtoF").checked==true)
+  resp.innerHTML = ctof(num);
+ if(document.getElementById("FtoC").checked==true)
+  resp.innerHTML = ftoc(num);
+}
+function ctof(x)
+{
+ return (1.8*x + 32);
+}
+
+function ftoc(x)
+{
+
+ return ( (x-32)*5/9 );
+}
